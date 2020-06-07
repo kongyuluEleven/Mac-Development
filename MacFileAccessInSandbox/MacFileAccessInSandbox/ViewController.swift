2805bf73f32b0643f2e6de303001d22de40ccd65
kongyulu <kongyl@rd.wondershare.cn>
2020-06-07 15:36:48 +0800

    finish save file

--- MacFileAccessInSandbox/MacFileAccessInSandbox/ViewController.swift
+++ MacFileAccessInSandbox/MacFileAccessInSandbox/ViewController.swift
@@ -8,8 +8,16 @@
 
 import Cocoa
 
+let LastSaveFilePathKey = "LastSaveFilePathKey"
+
 class ViewController: NSViewController {
 
+    @IBOutlet weak var btnOpen: NSButton!
+    @IBOutlet weak var btnSave: NSButton!
+    @IBOutlet weak var filePathField: NSTextField!
+    @IBOutlet weak var textScrollView: NSScrollView!
+    @IBOutlet var textV: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,5 +31,57 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func openFileAction(_ sender: Any) {
+        openFile()
+    }
+    
+    @IBAction func saveFileAction(_ sender: Any) {
+        saveFile()
+    }
+    
+}
+
+extension ViewController {
+    
+    /// 打开文件
+    fileprivate func openFile() {
+        let openPanel = NSOpenPanel()
+        openPanel.prompt = "Open"
+        openPanel.allowsMultipleSelection = true
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = true
+        openPanel.resolvesAliases = true
+        openPanel.nameFieldLabel = "Open File"
+        openPanel.title = "Open"
+        openPanel.allowedFileTypes = ["txt"]
+        openPanel.begin { (response) in
+            if response == .OK {
+                let urls = openPanel.urls
+                for url in urls {
+                    //UserDefaults.standard.set(url, forKey: LastSaveFilePathKey)
+                    UserDefaults.standard.set(url.path, forKey: LastSaveFilePathKey)
+                    UserDefaults.standard.synchronize()
+                    self.filePathField.stringValue = url.path
+                    
+                    let text = try? String(contentsOf: url, encoding: .utf8 )
+                    self.textV.string = text ?? ""
+                }
+            }
+        }
+    }
+    
+    
+    /// 保存文件
+    fileprivate func saveFile() {
+        guard let path = UserDefaults.standard.value(forKey: LastSaveFilePathKey) as? String else {
+            return
+        }
+        
+        let text =  textV.string
+        
+        let url = URL(fileURLWithPath: path)
+        try? text.write(to: url, atomically: true, encoding: .utf8)
+        
+    }
 }
 
