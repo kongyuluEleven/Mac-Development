e5e5c33af84d94eff4b317f47c5d675b00a43856
kongyulu <kongyl@rd.wondershare.cn>
2020-09-09 17:44:18 +0800

    增加统一平台工程

---
+++ Mac slic/KLrcCamera/KLrcCamera/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  KLrcCamera
+//
+//  Created by kongyulu on 2020/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var btnCamera: UIButton!
+    @IBOutlet weak var btnSiri: UIButton!
+    
+    @IBOutlet weak var btnRecognitionPicture: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnCameraClicked(_ sender: Any) {
+    }
+    
+    @IBAction func btnRecoginitionPictureClicked(_ sender: Any) {
+    }
+    
+    @IBAction func btnSiriClicked(_ sender: Any) {
+    }
+    
+}
+
---
+++ Mac slic/KLrcCamera/Pods/Target Support Files/Pods-KLrcCamera-KLrcCameraUITests/Pods-KLrcCamera-KLrcCameraUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KLrcCamera_KLrcCameraUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KLrcCamera_KLrcCameraUITests
+@end
