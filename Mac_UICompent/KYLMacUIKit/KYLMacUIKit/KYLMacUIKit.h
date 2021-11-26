6b14c7c1700113a37217568bae12998b856f08f5
kongyulu <kongyl@300624.cn>
2021-11-26 21:54:06 +0800

    增加mac UIDemo

---
+++ Mac_UICompent/KYLMacUIKit/KYLMacUIKit/KYLMacUIKit.h
@@ -0,0 +1,18 @@
+//
+//  KYLMacUIKit.h
+//  KYLMacUIKit
+//
+//  Created by kongyulu on 2021/11/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KYLMacUIKit.
+FOUNDATION_EXPORT double KYLMacUIKitVersionNumber;
+
+//! Project version string for KYLMacUIKit.
+FOUNDATION_EXPORT const unsigned char KYLMacUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYLMacUIKit/PublicHeader.h>
+
+
